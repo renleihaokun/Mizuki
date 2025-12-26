48c47cb0961fb2b7d045c83bf4a5f97eea2214b2
renleihaokun <119118549+renleihaokun@users.noreply.github.com>
2025-12-26 22:32:54 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "困",
 	subtitle: "并非demo",
-	siteURL: "https://blog.renleihaokun.dpdns.org/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://blog.haokun.me/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-12-02", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
